b6171d9f5c655404b6dfb178c4cf82e3b7c41f83
Anthony Rurup <tcrurup@gmail.com>
2019-04-26 03:09:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def oxford_comma(array)
-
+  
+  #If there is more that one word in the list then 'and' must be added before the final word of the list
+  if array.length > 1
+    array[array.length - 1] = "and #{array.last}"  
+  end
+  
+  #If there are more than two words than everything get's joined with a comma
+  if array.length <= 2 
+    array.join(" ")
+  else
+    array.join(", ")
+  end
+  
 end
