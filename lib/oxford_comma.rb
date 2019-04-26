0e13e61146930ffe6bdea1f764c1991c2bb77833
Anthony Rurup <tcrurup@gmail.com>
2019-04-26 02:48:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   
   #If there is more that one word in the list then 'and' must be added before the final word of the list
   if array.length > 1
-    array[array.length - 1] = " and #{array.last}"  
+    array[array.length - 1] = "and #{array.last}"  
   end
   
   #If there are more than two words than everything get's joined with a comma
